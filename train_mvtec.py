62b6c5a20fc024c083c5c923c1b3d4c3a53a3161
Ubuntu <ubuntu@ip-172-31-38-97.ec2.internal>
2020-01-07 17:51:29 +0000

    modified SSIM loss...needs further testing

--- train_mvtec.py
+++ train_mvtec.py
@@ -26,8 +26,8 @@ X_train_full = np.load("X_train.npy")
 
 X_train, X_valid = X_train_full[:-363], X_train_full[-363:]
 
-plt.imshow(X_train[1000])
-plt.show()
+# plt.imshow(X_train[1000])
+# plt.show()
 
 
 # ============================ DEFINE MODEL =================================
@@ -42,7 +42,7 @@ conv_encoder = keras.models.Sequential(
             strides=2,
             padding="SAME",
             activation=keras.layers.LeakyReLU(0.2),
-            input_shape=[256, 256, 3],
+            input_shape=[256, 256, 1],  # SSIM
         ),  # CONV0 (added layer)
         # keras.layers.MaxPool2D(pool_size=2),
         keras.layers.Conv2D(
@@ -182,7 +182,7 @@ conv_decoder = keras.models.Sequential(
             activation=keras.layers.LeakyReLU(0.2),
         ),
         keras.layers.Conv2DTranspose(
-            3,
+            1,  ## SSIM
             kernel_size=4,
             strides=2,
             padding="SAME",
@@ -199,30 +199,52 @@ conv_decoder.summary()
 conv_ae.summary()
 
 # =============================== TRAINING SETUP =================================
-batch_size = 32
-epochs = 50
+batch_size = 5  # 32
+epochs = 5
 data_augmentation = True
 flow_from_directory = False
-loss = "L2"
+loss = "SSIM"
 # num_predictions = 20
 
 # import tf.image.ssim as ssim
 # from tensorflow.python.ops.image_ops_impl import ssim
+max_value = 1.0
+
+
+# def custom_loss(max_value):
+#     def loss(y_true, y_pred):
+#         return tf.image.ssim(img1=y_true, img2=y_pred, max_val=max_value)
+
+#     return loss
+
+
+def ssim_loss(y_true, y_pred):
+    return -1 * tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
+
+
+# from keras.losses import mean_squared_error
+# from tf.image import ssim
+# from skimage.measure import compare_ssim as ssim
 
 if loss == "SSIM":
     # convert X_train and X_valid to grayscale (SSIM only works with grayscale)
-    X_train = tensorflow.python.ops.image_ops_impl.rgb_to_grayscale(X_train)
-    X_valid = X_train = tensorflow.python.ops.image_ops_impl.rgb_to_grayscale(X_valid)
-    # Set loss function as SSIM
-    loss_function = tensorflow.python.ops.image_ops_impl.ssim  # TEST this
+    # X_train = tf.image.rgb_to_grayscale(X_train).numpy()
+    # X_valid = tf.image.rgb_to_grayscale(X_valid).numpy()
+
+    X_train = tf.image.rgb_to_grayscale(X_train)
+    X_valid = tf.image.rgb_to_grayscale(X_valid)
+
+    # loss_function = tf.image.ssim  # TEST this
     optimizer = keras.optimizers.Adam(
         learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False
     )
     # compile model
     conv_ae.compile(
-        loss=loss_function,
+        # loss=custom_loss(max_value),
+        loss=ssim_loss,
         optimizer=optimizer,
-        metrics=[tensorflow.python.ops.image_ops_impl.ssim],
+        # metrics=[custom_loss(max_value)],
+        metrics=[ssim_loss, "mean_squared_error"],
     )
     # specify model name and directory to save model to
     model_name = "CAE_" + str(epochs) + "_datagen.h5"
@@ -243,11 +265,6 @@ else:
     # specify model name and directory to save model to
     model_name = "CAE_" + str(epochs) + "_datagen.h5"
     save_dir = os.path.join(os.getcwd(), "saved_models/l2_loss")
-
-
-# history = conv_ae.fit(X_train, X_train, epochs=50,
-#                       validation_data=[X_valid, X_valid], shuffle=True)
-# conv_ae.save('saved_models/l2_loss/ae_50.h5')
 
 
 # =============================== TRAINING =================================
@@ -303,7 +320,6 @@ else:
             target_size=(256, 256),
             color_mode="rgb",
             # classes=classes,  # does not work because of subdirectory architecture, see https://keras.io/preprocessing/image/#flow_from_directory
-            # possible solution: combine datagenerators, see https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator?version=stable
             batch_size=32,
             class_mode="binary",
         )
@@ -332,20 +348,51 @@ else:
 # ====================== SEE RESULTS OF TRAINED AE =======================
 
 # show reconstructed image sample--------------------------------
-img = X_train[100]
-fig, axs = plt.subplots(1, 2)
-axs[0].imshow(img)
 
-# expand dimension to one sample
-img_tensor = expand_dims(img, 0)
-img_reconstruction = conv_ae.predict(img_tensor)
-img_reconstruction = img_reconstruction[0]
-axs[1].imshow(img_reconstruction)
+
+def show_original_and_reconstructed_img_2(image, model):
+    fig, axes = plt.subplots(1, 2)
+    ax = axes.ravel()
+    # label = 'MSE: {:.2f}, SSIM: {:.2f}'
+
+    img1 = tf.convert_to_tensor(expand_dims(image, 0))
+    ax[0].imshow(img1.numpy()[0, :, :, 0], cmap=plt.cm.gray, vmin=0, vmax=1)
+    ax[0].set_title("Original image")
+
+    img2 = tf.convert_to_tensor(model.predict(expand_dims(image, 0)))
+    ax[1].imshow(img2.numpy()[0, :, :, 0], cmap=plt.cm.gray, vmin=0, vmax=1)
+    ax[1].set_title("Reconstructed image")
+
+    ssim_value = tf.image.ssim(img1=img1, img2=img2, max_val=1.0)
+    print("SSIM: {:.2f}".format(ssim_value.numpy()[0]))
+
+    plt.show()
+
+
+show_original_and_reconstructed_img_2(X_train[1900], conv_ae)
 
 import utils
 
 utils.show_original_and_reconstructed_img(X_train[100], conv_ae)  # FIX
+utils.show_original_and_reconstructed_img_2(X_train[100], conv_ae)  # FIX
 
+# img1 and img2 must be image tensors
+sim_value = tf.image.ssim(
+    img1=tf.convert_to_tensor(X_train[:4]),
+    img2=tf.convert_to_tensor(X_train[:4]),
+    max_val=1.0,
+)
+
+
+img1 = tf.convert_to_tensor(expand_dims(X_train[500], 0))
+plt.imshow(img1.numpy()[0, :, :, 0])
+img2 = tf.convert_to_tensor(conv_ae.predict(expand_dims(X_train[500], 0)))
+plt.imshow(img2.numpy()[0, :, :, 0])
+sim_value = tf.image.ssim(img1=img1, img2=img2, max_val=1.0)
+# returns <tf.Tensor: id=23418566, shape=(1,), dtype=float32, numpy=array([-0.72969997], dtype=float32)>
+
+# returns scalar value for similarity
+tf.image.ssim(img1=img1, img2=img2, max_val=1.0).numpy()[0]  # return -0.72969997
 
 # ----------------------------------------------------------------
 
