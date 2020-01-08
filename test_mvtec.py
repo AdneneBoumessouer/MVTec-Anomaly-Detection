71cd897e0d0008c11d3e2a930458950ddc2c6581
Adnene Boumessouer <adnen.boumessouer@gmail.com>
2020-01-08 15:05:26 +0100

    load trained model with ssim loss

--- test_mvtec.py
+++ test_mvtec.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
-
+from train_mvtec import ssim_loss as ssim_loss
 
 def residual_map(img1, img2):
     pass
@@ -13,6 +13,14 @@ conv_ae = keras.models.load_model(
     "saved_models/l2_loss/ae_50_datagen.h5",
     custom_objects={
         "LeakyReLU": keras.layers.LeakyReLU
+    },  # https://stackoverflow.com/questions/55364954/keras-load-model-cant-recognize-tensorflows-activation-functions
+)
+
+conv_ae = keras.models.load_model(
+    "saved_models/SSIM/CAE_5_datagen.h5",
+    custom_objects={
+        "LeakyReLU": keras.layers.LeakyReLU,
+        "ssim_loss": ssim_loss
     },  # https://stackoverflow.com/questions/55364954/keras-load-model-cant-recognize-tensorflows-activation-functions
 )
 
