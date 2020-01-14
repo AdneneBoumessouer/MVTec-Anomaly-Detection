db5227111e6cbeae6d923b639eba2d98e32dbcce
Adnene Boumessouer <adnen.boumessouer@gmail.com>
2020-01-14 22:29:18 +0100

    This script is still MESSY, it hasn't got any finesse

--- fine_tune.py
+++ fine_tune.py
@@ -12,15 +12,14 @@ import csv
 import pandas as pd
 import json
 
-"""THIS SCRIPT IS NOT YET COMPLETE"""
+"""THIS SCRIPT IS MESSY AND NOT YET COMPLETE"""
 
 
 
 # ================= LOAD TRAINED MODEL AND CORRESPONDING SETUP ===================
-model_path = "saved_models/MSSIM/11-01-2020_14:18:20/CAE_50_flow_from_dir_datagen.h5"
-conv_ae, train_setup, history = utils.load_trained_model(model_path)
+model_path = "saved_models/MSSIM/11-01-2020_14:18:20/CAE_e50_b4_0.h5"
+model, train_setup, history = utils.load_model_HDF5(model_path)
 
-# ====================== LOAD DATA ===============================
 
 # X_train, X_valid, X_test, y_test = utils.load_mvtec_data_as_tensor(
 #     dir_path="datasets/tensors", validation_split=0.1, numpy=False
@@ -32,22 +31,75 @@ validation_generator = validation_datagen.flow_from_directory(
                 directory="datasets/data/validation",
                 target_size=(256, 256),
                 color_mode=train_setup["color_mode"],
-                batch_size=1,
+                batch_size=train_setup["batch_size"],
                 class_mode="input",
             )
+# ====================== Experimentation ======================
+# try to train loaded model
 
-# this does not work
+
+# # set loss function, optimizer and metric
+# if train_setup["loss"] == "SSIM":
+#     loss_function = custom_loss_functions.ssim_loss
+
+#     optimizer = keras.optimizers.Adam(
+#         learning_rate=2e-4, beta_1=0.9, beta_2=0.999, amsgrad=False
+#     )
+#     model.compile(
+#         loss=loss_function,
+#         optimizer=optimizer,
+#         metrics=[loss_function, "mean_squared_error"],
+#     )
+
+# elif train_setup["loss"] == "MSSIM":
+#     loss_function = custom_loss_functions.mssim_loss
+#     optimizer = keras.optimizers.Adam(
+#         learning_rate=2e-4, beta_1=0.9, beta_2=0.999, amsgrad=False
+#     )
+#     model.compile(
+#         loss=loss_function,
+#         optimizer=optimizer,
+#         metrics=[loss_function, "mean_squared_error"],
+#     )
+
+# elif train_setup["loss"] == "MSE":
+#     loss_function = "mean_squared_error"
+#     optimizer = keras.optimizers.Adam(
+#         learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False
+#     )
+#     model.compile(
+#         loss=loss_function, optimizer=optimizer, metrics=["mean_squared_error"]
+#     )
+
+
+# history_test = model.fit_generator(
+#         generator=validation_generator,
+#         epochs=train_setup["epochs"],
+#         steps_per_epoch=validation_generator.samples // train_setup["batch_size"],
+#         workers=-1,
+#     )
+# =======================================================================
+model_path = "saved_models/MSSIM/11-01-2020_14:18:20/CAE_e50_b4_0.h5"
+model, train_setup, history = utils.load_model_HDF5(model_path)
+
+!mkdir -p saved_model
+model.save('saved_model/my_model')
+new_model = tf.keras.models.load_model('saved_model/my_model')
+
+
+
+# =======================================================================
 
 img = validation_generator.next()[0] 
-# img_pred = conv_ae.predict(img)
+# img_pred = model.predict(img)
 # plt.imshow(img_pred[0])
-utils.compare_images(img[0], model=conv_ae)
+utils.compare_images(img[0], model=model)
 
 # ====================== SEE RESULTS OF TRAINED AE =======================
 
 
 model_path=  "saved_models/SSIM/CAE_150_datagen.h5"
-CAE_ssim = keras.models.load_model(
+model = keras.models.load_model(
     filepath=model_path,
     custom_objects={
         "LeakyReLU": keras.layers.LeakyReLU,
@@ -66,41 +118,27 @@ validation_generator = validation_datagen.flow_from_directory(
             )
 
 img = validation_generator.next()[0] 
-# img_pred = conv_ae.predict(img)
+# img_pred = model.predict(img)
 # plt.imshow(img_pred[0])
-utils.compare_images(img[0], model=CAE_ssim)
+utils.compare_images(img[0], model=model)
 
 # this does not work
 
 img = validation_generator.next()[0] 
-# img_pred = conv_ae.predict(img)
+# img_pred = model.predict(img)
 # plt.imshow(img_pred[0])
-utils.compare_images(img[0], model=conv_ae)
+utils.compare_images(img[0], model=model)
 
 
 # with flow
 X_train_full = np.load("X_train.npy")
 importlib.reload(utils)
 
-utils.compare_images(X_train[100], X_train[101])
-utils.compare_images(X_train[100], X_train[500])
-utils.compare_images(X_train[100], X_train[700])
-utils.compare_images(X_train[100], X_train[1100])
-utils.compare_images(X_train[1101], X_train[1100])
 
-utils.compare_images(X_train[1500], model=conv_ae)
+utils.compare_images(X_train[1500], model=model)
 import matplotlib.image as mpimg
 
 img = mpimg.imread("datasets/data/train/good/bottle_000.png")
-
-
-utils.compare_images(X_train_full[100], X_train_full[101])
-utils.compare_images(X_train_full[100], X_train_full[500])
-utils.compare_images(X_train_full[100], X_train_full[700])
-utils.compare_images(X_train_full[100], X_train_full[1100])
-utils.compare_images(X_train_full[1101], X_train_full[1100])
-
-
 
 
 img_imread = mpimg.imread("datasets/data/train/good/bottle_000.png")  # scaled [0,1]
@@ -108,8 +146,8 @@ img_imread = mpimg.imread("datasets/data/train/good/bottle_000.png")  # scaled [
 
 # plot image, reconstruction and residual map
 img1 = X_train[1200]
-utils.compare_images(img1, model=conv_ae)
-img2 = conv_ae.predict(tf.expand_dims(img1, 0))[0]
+utils.compare_images(img1, model=model)
+img2 = model.predict(tf.expand_dims(img1, 0))[0]
 res_map = utils.residual_map_image(img1, img2)
 plt.imshow(res_map[:, :, 0], cmap=plt.cm.gray, vmin=0, vmax=1)
 
@@ -118,7 +156,7 @@ plt.imshow(res_map[:, :, 0], cmap=plt.cm.gray, vmin=0, vmax=1)
 
 # show encoder and decoder architecture
 
-# encoder = conv_ae(autoencoder.input, autoencoder.layers[-2].output)
+# encoder = model(autoencoder.input, autoencoder.layers[-2].output)
 
 # decoder_input = Input(shape=(encoding_dim,))
 # decoder = Model(decoder_input, autoencoder.layers[-1](decoder_input))
