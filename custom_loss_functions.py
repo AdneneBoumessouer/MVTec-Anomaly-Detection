2bb51b90fe8ebdd83cf2d45da4fd242a849ac118
Adnene Boumessouer <adnen.boumessouer@gmail.com>
2020-01-18 17:05:07 +0100

    added l2_loss function (slightly different from MSE)

--- custom_loss_functions.py
+++ custom_loss_functions.py
@@ -4,15 +4,18 @@ import keras.backend as K
 
 
 def ssim_loss(img_true, img_pred):
-    return (1 - K.mean(tf.image.ssim(img_true, img_pred, 1.0), axis=-1))/2
+    return (1 - K.mean(tf.image.ssim(img_true, img_pred, 1.0), axis=-1)) / 2
 
-
-# def mssim_loss(img_true, img_pred):
-#     return -1 * K.mean(tf.image.ssim_multiscale(img_true, img_pred, 1.0), axis=-1)
 
 def mssim_loss(img_true, img_pred):
     return 1 - K.mean(tf.image.ssim_multiscale(img_true, img_pred, 1.0), axis=-1)
 
+
+def l2_loss(img_true, img_pred):
+    return 2 * tf.nn.l2_loss(img_true - img_pred)
+
+
+# https://www.tensorflow.org/api_docs/python/tf/nn/l2_loss?hl=ko
 
 # --------------------------keep this as backup--------------------------------
 # max_value = 1.0
