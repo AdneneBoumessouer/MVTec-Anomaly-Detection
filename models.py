df76072b5a8fab2be04e742f8548397bad3c4702
Adnene Boumessouer <adnen.boumessouer@gmail.com>
2020-03-09 18:00:49 +0100

    removed unnecessary code

--- models.py
+++ models.py
@@ -195,50 +195,37 @@ def build_mvtec_2(channels=3):
     x = keras.layers.Conv2D(32, (4, 4), strides=2, activation="relu", padding="same")(
         input_img
     )
-    x = keras.layers.Conv2D(32, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(32, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(32, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(64, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(64, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (4, 4), strides=2, activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (4, 4), strides=2, activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (3, 3), strides=1, activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (4, 4), strides=2, activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (3, 3), strides=1, activation="relu", padding="same")(x)
     x = keras.layers.Conv2D(128, (4, 4), strides=2, activation="relu", padding="same")(
         x
     )
-    x = keras.layers.Conv2D(64, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(32, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (3, 3), strides=1, activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (3, 3), strides=1, activation="relu", padding="same")(x)
     encoded = keras.layers.Conv2D(1, (8, 8), strides=1, padding="same")(x)
 
     # Decode---------------------------------------------------------------------
     x = keras.layers.Conv2D(32, (3, 3), strides=1, activation="relu", padding="same")(
         encoded
     )
-    x = keras.layers.Conv2D(64, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (3, 3), strides=1, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
     x = keras.layers.Conv2D(128, (4, 4), strides=2, activation="relu", padding="same")(
         x
     )
     x = keras.layers.UpSampling2D((2, 2))(x)
-    x = keras.layers.Conv2D(64, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (3, 3), strides=1, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
-    x = keras.layers.Conv2D(64, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(64, (4, 4), strides=2, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
-    x = keras.layers.Conv2D(32, (3, 3), strides=1,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (3, 3), strides=1, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
-    x = keras.layers.Conv2D(32, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (4, 4), strides=2, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((4, 4))(x)
-    x = keras.layers.Conv2D(32, (4, 4), strides=2,
-                            activation="relu", padding="same")(x)
+    x = keras.layers.Conv2D(32, (4, 4), strides=2, activation="relu", padding="same")(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
     x = keras.layers.Conv2D(32, (8, 8), activation="relu", padding="same")(x)
 
@@ -372,98 +359,4 @@ def build_resnet():
 
 
 # ==================================================================
-# ==================================================================
 
-# def autoencoder_inception_resnet_v2_old(channels):
-
-#     # encoder
-#     base_encoder = keras.applications.inception_resnet_v2.InceptionResNetV2(
-#         include_top=False,
-#         weights="imagenet",
-#         input_tensor=None,
-#         input_shape=(299, 299, 3),
-#         pooling=None,
-#     )
-#     # print(base_encoder.summary())
-
-#     conv_encoder = keras.models.Sequential(
-#         [
-#             base_encoder,
-#             keras.layers.Conv2D(
-#                 512, kernel_size=1, strides=1, padding="SAME", activation=None,
-#             ),
-#             keras.layers.Conv2D(
-#                 512, kernel_size=1, strides=1, padding="SAME", activation=None,
-#             ),
-#         ]
-#     )
-#     print(conv_encoder.summary())
-
-#     # decoder
-#     inputs = keras.layers.Input(shape=tuple(conv_encoder.output.shape[-3:]))
-
-#     layer_1 = keras.layers.Conv2DTranspose(
-#         512, kernel_size=4, strides=2, padding="SAME", activation=None,
-#     )(inputs)
-#     layer_2 = keras.layers.BatchNormalization()(layer_1)
-#     layer_3 = keras.layers.ReLU()(layer_2)
-#     layer_4 = keras.layers.Conv2DTranspose(
-#         512, kernel_size=3, strides=1, padding="SAME", activation=None,
-#     )(layer_3)
-#     layer_5 = keras.layers.BatchNormalization()(layer_4)
-#     layer_6 = keras.layers.ReLU()(layer_5)
-#     ####
-#     add_1 = keras.layers.Add()([layer_1, layer_6])
-#     ####
-#     layer_7 = keras.layers.Conv2DTranspose(
-#         256, kernel_size=4, strides=2, padding="SAME", activation=None,
-#     )(add_1)
-#     layer_8 = keras.layers.BatchNormalization()(layer_7)
-#     layer_9 = keras.layers.ReLU()(layer_8)
-#     layer_10 = keras.layers.Conv2DTranspose(
-#         256, kernel_size=3, strides=1, padding="SAME", activation=None,
-#     )(layer_9)
-#     layer_11 = keras.layers.BatchNormalization()(layer_10)
-#     layer_12 = keras.layers.ReLU()(layer_11)
-#     ####
-#     add_2 = keras.layers.Add()([layer_7, layer_12])
-#     ####
-#     layer_13 = keras.layers.Conv2DTranspose(
-#         128, kernel_size=4, strides=2, padding="SAME", activation=None,
-#     )(add_2)
-#     layer_14 = keras.layers.BatchNormalization()(layer_13)
-#     layer_15 = keras.layers.ReLU()(layer_14)
-#     layer_16 = keras.layers.Conv2DTranspose(
-#         128, kernel_size=3, strides=1, padding="SAME", activation=None,
-#     )(layer_15)
-#     layer_17 = keras.layers.BatchNormalization()(layer_16)
-#     layer_18 = keras.layers.ReLU()(layer_17)
-#     ####
-#     add_3 = keras.layers.Add()([layer_13, layer_18])
-#     ####
-#     layer_19 = keras.layers.Conv2DTranspose(
-#         64, kernel_size=4, strides=2, padding="SAME", activation=None,
-#     )(add_3)
-#     layer_20 = keras.layers.BatchNormalization()(layer_19)
-#     layer_21 = keras.layers.ReLU()(layer_20)
-#     layer_22 = keras.layers.Conv2DTranspose(
-#         64, kernel_size=3, strides=1, padding="SAME", activation=None,
-#     )(layer_21)
-#     layer_23 = keras.layers.BatchNormalization()(layer_22)
-#     layer_24 = keras.layers.ReLU()(layer_23)
-#     ####
-#     add_4 = keras.layers.Add()([layer_19, layer_24])
-#     ####
-#     layer_25 = keras.layers.Conv2DTranspose(
-#         3,
-#         kernel_size=4,
-#         strides=2,
-#         padding="SAME",
-#         activation=keras.activations.sigmoid,
-#     )(add_4)
-
-#     conv_decoder = keras.models.Model(inputs=inputs, outputs=layer_25)
-
-#     model = keras.models.Sequential([conv_encoder, conv_decoder])
-
-#     return model
